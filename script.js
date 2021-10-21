d2a097a55a2ac02c0a208267581214f9b5b4d858
ibra <ibrakh80@gamail.com>
2021-10-21 17:03:02 +0300

    Change style and script

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const nav = document.getElementById("nav");
-
+const dark = document.getElementById("dark");
 let start = 0;
 
 
 function wallChange() {
+    console.log(dark);
     if (start === 0) {
+        dark.classList.remove("changeA");
+        dark.classList.add("changeD");
         document.getElementById("parent").style.backgroundImage = "url(https://ibrakh.github.io/SCH/main.jpg)";
         document.getElementById("small").style.background = "linear-gradient(45deg, rgb(223, 181, 126), rgb(172, 131, 131))";
         start++
     } else {
+        dark.classList.remove("changeD");
+        dark.classList.add("changeA");
         document.getElementById("parent").style.backgroundImage = "url(https://ibrakh.github.io/SCH/secend.jpg)";
         document.getElementById("small").style.background = "linear-gradient(45deg, rgb(255, 0, 255), rgb(97, 95, 211))";
         start--
